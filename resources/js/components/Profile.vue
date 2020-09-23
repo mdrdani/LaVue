5ac6d3fb3bb2e4611edbc8f885c7280821914cf7
mdrdani <sudokill3004@gmail.com>
2020-09-23 20:18:03 +0700

    Profile Components for User

--- resources/js/components/Profile.vue
+++ resources/js/components/Profile.vue
@@ -1,12 +1,62 @@
+<style>
+.widget-user-header {
+    background-position: center center;
+    background-size: cover;
+}
+</style>
+
 <template>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Profile Component</div>
-
-                    <div class="card-body">
-                        I'm an Profile component.
+        <div class="row">
+            <div class="col-md-12 mt-4">
+                <div class="card card-widget widget-user">
+                    <!-- Add the bg color to the header using any of the bg-* classes -->
+                    <div
+                        class="widget-user-header text-white"
+                        style="background-image:url('./img/user-cover.jpg'); height:300px"
+                    >
+                        <h3 class="widget-user-username text-right">
+                            Elizabeth Pierce
+                        </h3>
+                        <h5 class="widget-user-desc text-right">
+                            Web Designer
+                        </h5>
+                    </div>
+                    <div class="widget-user-image">
+                        <img class="img-circle" src="" alt="User Avatar" />
+                    </div>
+                    <div class="card-footer">
+                        <div class="row">
+                            <div class="col-sm-4 border-right">
+                                <div class="description-block">
+                                    <h5 class="description-header">3,200</h5>
+                                    <span class="description-text">SALES</span>
+                                </div>
+                                <!-- /.description-block -->
+                            </div>
+                            <!-- /.col -->
+                            <div class="col-sm-4 border-right">
+                                <div class="description-block">
+                                    <h5 class="description-header">13,000</h5>
+                                    <span class="description-text"
+                                        >FOLLOWERS</span
+                                    >
+                                </div>
+                                <!-- /.description-block -->
+                            </div>
+                            <!-- /.col -->
+                            <div class="col-sm-4">
+                                <div class="description-block">
+                                    <h5 class="description-header">35</h5>
+                                    <span class="description-text"
+                                        >PRODUCTS</span
+                                    >
+                                </div>
+                                <!-- /.description-block -->
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                        <!-- /.row -->
                     </div>
                 </div>
             </div>
@@ -15,7 +65,5 @@
 </template>
 
 <script>
-    export default {
-        
-    }
+export default {};
 </script>
